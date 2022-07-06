afacf1f7a811366388e087f2798641b7d7c74587
Benjamin Swart <Benjaminswart@email.cz>
2022-07-06 16:34:50 +0200

    Remove unneeded import

--- client/src/login/LoginInterface.tsx
+++ client/src/login/LoginInterface.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import { LoginState } from "types/loginState";
 import Spinner from "utils/Spinner";
 import { useSocket } from "utils/useSocket";
